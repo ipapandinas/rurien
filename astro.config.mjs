706b26037145bf2a2a693854d4349263f5f128e5
Igor Papandinas <igor.papandinas@posteo.net>
2023-10-23 00:44:02 +0200

    vercel adapter aadded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  // PORT 
+  // PORT
   server: {
-    port: 3000
+    port: 3000,
   },
-  integrations: [react(), tailwind(
-    {
-      applyBaseStyles: false
-    }
-  )]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  output: "static",
+  adapter: vercelStatic(),
 });
